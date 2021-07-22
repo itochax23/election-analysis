34f743801f97ed67eaaa95984745ebcf4a7ac20b
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-07-22 18:38:54 -0500

    Finish printing

--- PyPoll.py
+++ PyPoll.py
@@ -47,16 +47,15 @@ with open(file_to_save, "w") as txt_file:
     # Save the final vote count to the text file.
     txt_file.write(election_results)
 
-
-
     for candidate_name in candidate_votes:
         # Retrieve vote count and percentage.
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
         # Print each candidate, their voter count, and percentage to the
         # terminal.
-        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
         # Determine winning vote count, winning percentage, and candidate.
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
@@ -71,4 +70,5 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n")
 
     # print(winning_candidate_summary)
-
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
