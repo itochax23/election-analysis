9fa233429c6d50382011d0fe2102d998428f013e
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-07-22 18:01:17 -0500

    Find candidate votes

--- PyPoll.py
+++ PyPoll.py
@@ -17,6 +17,12 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # 1. Initialize a total vote counter.
 total_votes = 0
 
+#Declare a new blank list for the names of the candidates.
+candidate_options = []
+
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     # Read the file object with the reader function.
@@ -30,5 +36,25 @@ with open(file_to_load) as election_data:
         # 2. Add to the total vote count.
         total_votes += 1
 
+        # Print the candidate name from each row while we're using this existing loop.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
+
+# Print the candidate list.
+# print(candidate_options)
+
 # 3. Print the total votes.
-print(total_votes)
+# print(total_votes)
