f90bb7150f730ba77db257b896483c94f52949fd
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-07-25 19:12:20 -0500

    Create Python_practice.py

---
+++ Python_practice.py
@@ -0,0 +1,28 @@
+#3.2.2 Execute Python Files
+# print("Hello World")
+# type(3)
+# ballots = 1,341
+# print(ballots)
+# type(ballots)
+# num_candidates = 3
+# winning_percentage = 73.81
+# candidate = "Diane"
+# won_election = True
+# # help("keywords")
+# counties = ["Arapahoe","Denver","Jefferson"]
+# counties[0]
+# print(counties[0])
+# print(counties[-1])
+# len(counties)
+# counties.append("El Paso")
+# counties.insert(2, "El Paso")
+# print(counties)
+# counties.remove("El Paso")
+# counties.pop(3)
+# print(counties)
+
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+print(len(counties_tuple))
+print(counties_tuple[1])
+
+counties_dict = {}
